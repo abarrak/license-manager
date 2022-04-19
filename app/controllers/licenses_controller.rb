969e49bc7da8219bc7cf3b5ed4343da5904ccfb4
Abdullah Barrak <a@abarrak.com>
2022-04-20 00:10:08 +0300

    fix licenses controller tests

--- app/controllers/licenses_controller.rb
+++ app/controllers/licenses_controller.rb
@@ -28,7 +28,7 @@ class LicensesController < ApplicationController
         format.html { redirect_to license_url(@license), notice: "License was successfully created." }
         format.json { render :show, status: :created, location: @license }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :new, status: :unprocessable_entity, error: "An error occurred during license creation." }
         format.json { render json: @license.errors, status: :unprocessable_entity }
       end
     end
@@ -49,22 +49,27 @@ class LicensesController < ApplicationController
 
   # DELETE /licenses/1 or /licenses/1.json
   def destroy
-    @license.destroy
-
     respond_to do |format|
-      format.html { redirect_to licenses_url, notice: "License was successfully destroyed." }
-      format.json { head :no_content }
+      if @license.destroy
+        flash[:notice] = "License was deleted successfully."
+        format.html { redirect_to licenses_url }
+        format.json { head :no_content }
+      else
+        flash[:error] = "Deleting license failed. Please try again."
+        format.html { redirect_to licenses_url }
+        format.json { head :no_content }
+      end
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_license
-      @license = License.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_license
+    @license = License.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def license_params
-      params.require(:license).permit(:title, :current_expire_date, :renewal_times, :owner, :description)
-    end
+  # Only allow a list of trusted parameters through.
+  def license_params
+    params.require(:license).permit(:title, :current_expire_date, :renewal_times, :owner, :description)
+  end
 end
--- test/controllers/licenses_controller_test.rb
+++ test/controllers/licenses_controller_test.rb
@@ -3,46 +3,69 @@ require "test_helper"
 class LicensesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @license = licenses(:one)
+    @sample_params = { license: { renewal_times: 7, title: "New SQL 2022" } }
   end
 
-  test "should get index" do
+  test "should give the licenses index correctly" do
     get licenses_url
     assert_response :success
+    assert_template 'index'
+    License.all.each do |post|
+      assert_select "a[href=?]", license_path(post)
+    end
   end
 
-  test "should get new" do
+  test "should get new page" do
     get new_license_url
     assert_response :success
+    assert_template :new
   end
 
-  test "should create license" do
+  test "successful create with correct credentials and data" do
     assert_difference("License.count") do
-      post licenses_url, params: { license: { current_expire_date: @license.current_expire_date, description: @license.description, owner: @license.owner, renewal_times: @license.renewal_times, title: @license.title } }
+      post licenses_url, params: {
+        license: {
+          current_expire_date: "2025-1-1", description: "AWS Subscription",
+          owner: "SRE", renewal_times: 0, title: "AWS"
+        }
+      }
     end
-
-    assert_redirected_to license_url(License.last)
+    new_license = License.last
+    assert_redirected_to license_url(new_license)
+    assert_equal "AWS", new_license.title
+    assert_equal 0, new_license.renewal_times
+    assert_equal "SRE", new_license.owner
+    assert_nil new_license.owner_email
   end
 
-  test "should show license" do
+  test "should display license page when show is clicked" do
     get license_url(@license)
     assert_response :success
+    assert_template :show
   end
 
-  test "should get edit" do
+  test "should get the editing page for a license" do
     get edit_license_url(@license)
     assert_response :success
+    assert_template :edit
   end
 
-  test "should update license" do
-    patch license_url(@license), params: { license: { current_expire_date: @license.current_expire_date, description: @license.description, owner: @license.owner, renewal_times: @license.renewal_times, title: @license.title } }
+  test "successful update with correct credentials and data" do
+    patch license_url(@license), params: @sample_params
     assert_redirected_to license_url(@license)
+    assert_not flash[:notice].empty?
+
+    @license.reload
+    assert_equal @license.title, @sample_params[:license][:title]
+    assert_equal @license.renewal_times, @sample_params[:license][:renewal_times]
   end
 
-  test "should destroy license" do
-    assert_difference("License.count", -1) do
+  test "successful destroy with correct credentials and data" do
+    assert_difference "License.count", -1 do
       delete license_url(@license)
     end
 
     assert_redirected_to licenses_url
+    assert_not_empty flash[:notice]
   end
 end
